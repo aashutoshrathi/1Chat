5ce3986778e2a94affe5d86e94d4212c296f6f0b
aashutoshrathi <aashutoshrathi@gmail.com>
2018-12-16 14:29:10 +0530

    Made Drawer with image and Linted code

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,37 @@ class MainWidget extends StatelessWidget {
           title: Text('Awesome Chat App'),
         ),
         body: ChatList(),
-        bottomNavigationBar: BottomBar()
+        bottomNavigationBar: BottomBar(),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                child: DrawerHeader(
+                    child: Center(
+                        child: Container(
+                      color: Colors.black38,
+                      padding: EdgeInsets.all(30.0),
+                      child: Image.network(
+                          'https://github.com/cerebro-iiitv/cerebro-web/blob/dev/public/fest-logo.png?raw=true'),
+                    )),
+                    margin: EdgeInsets.all(0.0),
+                    padding: EdgeInsets.all(0.0)),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_box),
+                title: Text('Login'),
+              ),
+              ListTile(
+                leading: Icon(Icons.exit_to_app),
+                title: Text('Logout'),
+              )
+            ],
+          ),
+        ),
       ),
       theme: ThemeData.dark(),
     );
@@ -26,26 +56,29 @@ class ChatList extends StatelessWidget {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('chat_133').snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError)
-          return new Text('Error: ${snapshot.error}');
+        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
         switch (snapshot.connectionState) {
-          case ConnectionState.waiting: return new Text('Loading...');
+          case ConnectionState.waiting:
+            return new Text('Loading...');
           default:
             return new ListView(
-              children: snapshot.data.documents.map((DocumentSnapshot document) {
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
                 return Card(
-                  elevation: 2.0,
-                  child: ListTile(
-                    onTap: () => debugPrint("Tapped messages from ${document['title']}"),               
-                    leading: Icon(Icons.account_circle, size:35),
-                    title: new Text(document['title']),
-                    subtitle: new Text(document['author']),
-                    trailing: GestureDetector(
-                      child: Icon(Icons.delete_sweep, color: Colors.redAccent, size:30),
-                      onTap: () => debugPrint("Hi Baka, Don't delete it."),
-                    ),
-                  )
-                );
+                    elevation: 2.0,
+                    color: Colors.black26,
+                    child: ListTile(
+                      onTap: () => debugPrint(
+                          "Tapped messages from ${document['title']}"),
+                      leading: Icon(Icons.account_circle, size: 35),
+                      title: new Text(document['title']),
+                      subtitle: new Text(document['author']),
+                      trailing: GestureDetector(
+                        child: Icon(Icons.delete_sweep,
+                            color: Colors.redAccent, size: 30),
+                        onTap: () => debugPrint("Hi Baka, Don't delete it."),
+                      ),
+                    ));
               }).toList(),
             );
         }
@@ -69,9 +102,7 @@ class BottomBar extends StatelessWidget {
           title: new Text('Messages'),
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          title: Text('Profile')
-        )
+            icon: Icon(Icons.person), title: Text('Profile'))
       ],
     );
   }
